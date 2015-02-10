58ff8cf0ceae00d8cdaf400c831d32d64ecfae31
liffeyd <declan@dws.ie>
2015-02-10 16:26:48 +0000

    DNN Delete Unauthorised Users
    Delete unauthorised users on a scheduled task.

---
+++ DNN-DeleteUnauthorisedUsers/Data/DataProvider.cs
@@ -0,0 +1,67 @@
+﻿using System.Data;
+using System;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Framework.Providers;
+
+namespace dwsDeleteSpamUsers.Data
+{
+    public abstract class DataProvider
+    {
+
+        #region Shared/Static Methods
+
+        private static DataProvider provider;
+
+        // return the provider
+        public static DataProvider Instance()
+        {
+            if (provider == null)
+            {
+                const string assembly = "dws.Modules.dwsDeleteSpamUsers.Data.SqlDataprovider,dwsDeleteSpamUsers";
+                Type objectType = Type.GetType(assembly, true, true);
+
+                provider = (DataProvider)Activator.CreateInstance(objectType);
+                DataCache.SetCache(objectType.FullName, provider);
+            }
+
+            return provider;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate", Justification = "Not returning class state information")]
+        public static IDbConnection GetConnection()
+        {
+            const string providerType = "data";
+            ProviderConfiguration _providerConfiguration = ProviderConfiguration.GetProviderConfiguration(providerType);
+
+            Provider objProvider = ((Provider)_providerConfiguration.Providers[_providerConfiguration.DefaultProvider]);
+            string _connectionString;
+            if (!String.IsNullOrEmpty(objProvider.Attributes["connectionStringName"]) && !String.IsNullOrEmpty(System.Configuration.ConfigurationManager.AppSettings[objProvider.Attributes["connectionStringName"]]))
+            {
+                _connectionString = System.Configuration.ConfigurationManager.AppSettings[objProvider.Attributes["connectionStringName"]];
+            }
+            else
+            {
+                _connectionString = objProvider.Attributes["connectionString"];
+            }
+
+            IDbConnection newConnection = new System.Data.SqlClient.SqlConnection();
+            newConnection.ConnectionString = _connectionString.ToString();
+            newConnection.Open();
+            return newConnection;
+        }
+
+        #endregion
+
+        #region Abstract methods
+
+        //public abstract IDataReader GetItems(int userId, int portalId);
+
+        //public abstract IDataReader GetItem(int itemId); 
+
+        public abstract void DeleteItems(int portalId);
+
+        #endregion
+
+    }
+
+}
---
+++ DNN-DeleteUnauthorisedUsers/Providers/DataProviders/SqlDataProvider/SqlDataProvider.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Framework.Providers;
+
+namespace dwsDeleteSpamUsers.Data
+{
+    class SqlDataProvider : DataProvider
+    {
+        #region Private Members
+
+        private const string ProviderType = "data";
+        private const string ModuleQualifier = "dws_";
+
+        private readonly ProviderConfiguration _providerConfiguration = ProviderConfiguration.GetProviderConfiguration(ProviderType);
+        private readonly string _connectionString;
+        private readonly string _providerPath;
+        private readonly string _objectQualifier;
+        private readonly string _databaseOwner;
+
+        #endregion
+
+        #region Constructors
+
+        public SqlDataProvider()
+        {
+
+            // Read the configuration specific information for this provider
+            Provider objProvider = (Provider)(_providerConfiguration.Providers[_providerConfiguration.DefaultProvider]);
+
+            // Read the attributes for this provider
+
+            //Get Connection string from web.config
+            _connectionString = Config.GetConnectionString();
+
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                // Use connection string specified in provider
+                _connectionString = objProvider.Attributes["connectionString"];
+            }
+
+            _providerPath = objProvider.Attributes["providerPath"];
+
+            _objectQualifier = objProvider.Attributes["objectQualifier"];
+            if (!string.IsNullOrEmpty(_objectQualifier) && _objectQualifier.EndsWith("_", StringComparison.Ordinal) == false)
+            {
+                _objectQualifier += "_";
+            }
+
+            _databaseOwner = objProvider.Attributes["databaseOwner"];
+            if (!string.IsNullOrEmpty(_databaseOwner) && _databaseOwner.EndsWith(".", StringComparison.Ordinal) == false)
+            {
+                _databaseOwner += ".";
+            }
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+        }
+
+        public string ProviderPath
+        {
+            get
+            {
+                return _providerPath;
+            }
+        }
+
+        public string ObjectQualifier
+        {
+            get
+            {
+                return _objectQualifier;
+            }
+        }
+
+        public string DatabaseOwner
+        {
+            get
+            {
+                return _databaseOwner;
+            }
+        }
+
+        // used to prefect your database objects (stored procedures, tables, views, etc)
+        private string NamePrefix
+        {
+            get { return DatabaseOwner + ObjectQualifier + ModuleQualifier; }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static object GetNull(object field)
+        {
+            return Null.GetNull(field, DBNull.Value);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        //public override IDataReader GetItem(int itemId)
+        //{
+        //    return SqlHelper.ExecuteReader(ConnectionString, NamePrefix + "spGetItem", itemId);
+        //}
+
+        //public override IDataReader GetItems(int userId, int portalId)
+        //{
+        //    return SqlHelper.ExecuteReader(ConnectionString, NamePrefix + "spGetItemsForUser", userId, portalId);
+        //}
+
+        public override void DeleteItems(int portalId)
+        {
+            return DataProvider.Instance().ExecuteNonQuery("dwsDeleteSpamUsers", portalId);
+        }
+
+
+        #endregion
+
+    }
+}
